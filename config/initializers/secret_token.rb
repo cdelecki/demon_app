71a33a49c4eecf36ce57c970109a648f12c65773
Chelsie Delecki <chelsie.delecki@gmail.com>
2013-08-27 15:10:36 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemonApp::Application.config.secret_key_base = 'baa5b5de0f0af9f1cca5ed981d9d21d19ea052bc8a316a843e92db4351e27d151e845ce583921bc6b8435216a720024730d231524ece15566d088ee2dedfeb84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemonApp::Application.config.session_store :cookie_store, key: '_demon_app_session'
